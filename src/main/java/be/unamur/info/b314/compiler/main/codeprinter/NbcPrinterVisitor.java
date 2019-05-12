fd7b3cda1a662134e5a746bee0bf8b25e79da048
baylib <benjamin.jonard@protonmail.com>
2019-05-12 15:57:17 +0200

    correction bugs + visitor Execution Function VarDecl InitVar

--- src/main/java/be/unamur/info/b314/compiler/main/codeprinter/NbcPrinterVisitor.java
+++ src/main/java/be/unamur/info/b314/compiler/main/codeprinter/NbcPrinterVisitor.java
@@ -4,6 +4,7 @@ import be.unamur.info.b314.compiler.PlayPlusBaseListener;
 import be.unamur.info.b314.compiler.PlayPlusBaseVisitor;
 import be.unamur.info.b314.compiler.PlayPlusParser;
 import be.unamur.info.b314.compiler.main.symboltable.Helpers.Expression;
+import be.unamur.info.b314.compiler.main.symboltable.Helpers.FunctionDecl;
 import be.unamur.info.b314.compiler.main.symboltable.SymbolTable;
 import be.unamur.info.b314.compiler.main.symboltable.contracts.Scope;
 import be.unamur.info.b314.compiler.main.symboltable.contracts.Type;
@@ -25,12 +26,14 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
     private SymbolTable symbolTable;
     private StringBuilder code;
     private HashMap<String, STGroup> templates;
+    private ArrayList<FunctionDecl> functionDecls;
 
     public NbcPrinterVisitor(String fileName, SymbolTable symtable) {
         this.fileName = fileName;
         this.symbolTable = symtable;
         initTemplates();
         this.code = new StringBuilder();
+        this.functionDecls = new ArrayList<>();
     }
 
     private void initTemplates() {
@@ -40,20 +43,37 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
 
     }
 
+    private void addFunctionDecl(FunctionDecl functionDecl) {
+        this.functionDecls.add(functionDecl);
+    }
+
+    @Override
+    public Integer visitRoot(PlayPlusParser.RootContext ctx) {
+        int value = 0;
+        symbolTable.setCurrentScope(symbolTable.getGlobals());
+        visitChildren(ctx);
+        symbolTable.setCurrentScopeToEnclosingOne();
+        return value;
+
+    }
+
     @Override
     public Object visitMainProgram(PlayPlusParser.MainProgramContext ctx) {
 
-        Optional<Symbol> result = symbolTable.getCurrentScope().resolveByName("main");
-        if (result.isPresent()){
-            this.symbolTable.setCurrentScope(((FunctionSymbol) result.get()).getBody());
-        }
-        return super.visitMainProgram(ctx);
+
+        //Optional<Symbol> result = symbolTable.getCurrentScope().resolveByName("main");
+        FunctionSymbol mainscope = (FunctionSymbol) symbolTable.getScopes().get(ctx);
+        System.out.println("BEFORE SET SCOPE :" +symbolTable.getCurrentScope().getScopeName());
+        this.symbolTable.setCurrentScope(mainscope);
+        System.out.println("AFTER SET SCOPE : "  + symbolTable.getCurrentScope().getScopeName());
+        return visitChildren(ctx);
+
     }
 
     @Override
     public Integer visitExprEnt(PlayPlusParser.ExprEntContext ctx) {
         String debugString = ctx.getText();
-        //System.out.println("VisitExprEnt :" + debugString );
+        System.out.println("VisitExprEnt :" + debugString );
         String op = "";
         int value = 0;
 
@@ -82,6 +102,13 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
             return value;
         }
 
+        if(ctx.funcCall() != null){
+
+
+            value = visitFuncCall(ctx.funcCall());// value of execution.
+            //value = symbolTable.getCurrentScope().resolveByName(ctx.funcCall().getText()).get().getValue();
+
+        }
 
         try {
 
@@ -90,11 +117,12 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
 
         } catch (Exception exception){
 
-            System.out.println("Aller voir dans la table avant de cast :" + exception.toString());
+            //System.out.println("Aller voir dans la table avant de cast :" + exception.toString());
 
             Optional<Symbol> result = symbolTable.getCurrentScope().resolveByName(ctx.getText());
 
             if (result.isPresent()) {
+                System.out.println("result get"+result.get());
                 System.out.println("valueof "+ctx.getText() + " value :"+result.get().getValue());
                 //result.get().getValue();
                 if (result.get().getValue() != null){
@@ -103,6 +131,7 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
                 }
 
             } else {
+                System.out.println("Scope ExprEnt : "+ symbolTable.getCurrentScope().getScopeName());
                 System.out.println("ERROR : valueof "+ctx.getText() +"");
 
             }
@@ -112,32 +141,46 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
         return value;
     }
 
+    @Override
+    public Object visitVarDecl(PlayPlusParser.VarDeclContext ctx) {
 
+        System.out.println("visitVarDecl :"+ctx.getText());
+       Iterator subVarDecls = ctx.subVarDecl().listIterator();
+       while (subVarDecls.hasNext()){
+           PlayPlusParser.SubVarDeclContext subVarDecl = (PlayPlusParser.SubVarDeclContext) subVarDecls.next();
+           int value = this.visitSubVarDecl(subVarDecl);
+           symbolTable.getCurrentScope().resolveByName(subVarDecl.ID().getText()).get().setValue(value);
+       }
 
+       return 0;
+    }
 
-        @Override
-        public Integer visitSubVarDecl(PlayPlusParser.SubVarDeclContext ctx) {
-            int value = 0;
-            System.out.println("visitSubVarDecl :"+ctx.getText());
-            String varName = ctx.ID().getText();
-            if (ctx.initVariable() != null) {
-                value = this.visitInitVariable(ctx.initVariable());
-                Optional<Symbol> result = symbolTable.getCurrentScope().resolveByName(varName);
-                if (result.isPresent()){
+    @Override
+    public Integer visitSubVarDecl(PlayPlusParser.SubVarDeclContext ctx) {
+        int value = 0;
+        System.out.println("visitSubVarDecl :"+ctx.getText());
+        //System.out.println(symbolTable.getCurrentScope());
+        String varName = ctx.ID().getText();
 
-                    result.get().setValue(value);
+        if (ctx.initVariable() != null) {
+            value = this.visitInitVariable(ctx.initVariable());
+            Optional<Symbol> result = resolveSymbolRec(varName,symbolTable.getCurrentScope());
+            if (result.isPresent()){
 
-                }
+                result.get().setValue(value);
+
             }
+        }
 
             return value;
-        }
+    }
 
     @Override
     public Integer visitInitVariable(PlayPlusParser.InitVariableContext ctx) {
         int value = 0;
         if (ctx.exprEnt() != null)
         { value = visitExprEnt(ctx.exprEnt());}
+
         return value;
 
     }
@@ -148,12 +191,15 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
         System.out.println("VisitAffectInstr :"+ctx.getText());
         if (ctx.exprG().ID() != null){
             String varName = ctx.exprG().ID().getText();
+            System.out.println("Scope Affect : "+ symbolTable.getCurrentScope().getScopeName());
             Optional<Symbol> result = symbolTable.getCurrentScope().resolveByName(varName);
+
             value = this.visitExprD(ctx.exprD());
             if (result.isPresent()) {
-                System.out.println("setting value of  "+ ctx.getText() + "value : "+ value);
+                System.out.println("setting value of  "+ varName + " in " + ctx.getText() + "value : "+ value);
 
                 result.get().setValue(value);
+
             } else {
                 System.out.println("ERROR : valueof "+ctx.getText() +"");
             }
@@ -169,6 +215,77 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
         int value = 0;
         if (ctx.exprEnt() != null)
         { value = visitExprEnt(ctx.exprEnt());}
+
+        return value;
+    }
+
+    @Override
+    public Integer visitFuncDecl(PlayPlusParser.FuncDeclContext ctx) {
+        System.out.println("VisitFuncDecl : "+ ctx.getText());
+        int value = 0;
+        String funcName = ctx.ID().getText();
+        FunctionDecl fct = new FunctionDecl(funcName ,ctx);
+        addFunctionDecl(fct);
+
+
+
+        if (ctx.returnInstr().exprD() != null) {
+            value = visitExprD(ctx.returnInstr().exprD());
+            System.out.println("TEst = " + value);
+        }
+
+        if (ctx.returnInstr().exprG() != null && ctx.returnInstr().exprG().ID() != null){
+            System.out.println("rtrninst ID :" + ctx.returnInstr().exprG().ID() );
+            //System.out.println("current scope : "+ symbolTable.getCurrentScope().getScopeName());
+            //System.out.println("current scope : "+ symbolTable.getCurrentScope());
+            String varName = ctx.returnInstr().exprG().ID().getText();
+            Optional<Symbol> result = symbolTable.getCurrentScope().resolveByName(varName);
+            if (result.isPresent()){
+                //value = result.get().setValue();
+                //value = result.get().getValue();
+            }
+        }
+
+        System.out.println("out");
+
+
+        return value;
+    }
+
+    @Override
+    public Integer visitFuncCall(PlayPlusParser.FuncCallContext ctx) {
+        int value = 0;
+        System.out.println("VisitFuncCall : "+ ctx.getText());
+        String funcName = ctx.ID().getText();
+
+        Optional<Symbol> result = symbolTable.getGlobals().resolveByName(funcName);
+        Scope currentScope = symbolTable.getCurrentScope();
+        if (result.isPresent()){
+            this.symbolTable.setCurrentScope(((FunctionSymbol) result.get()));
+            Iterator callargs = ctx.funcCallArgs().funcCallArg().listIterator();
+            int i = 0;
+            while (callargs.hasNext()){
+                PlayPlusParser.ExprDContext exprDContext = ((PlayPlusParser.FuncCallArgContext) callargs.next()).exprD();
+                ((FunctionSymbol) result.get()).getArgs().get(i).setValue(visitExprD(exprDContext));
+                i++;
+            }
+
+            PlayPlusParser.FuncDeclContext funcDeclContext = findFunctionDeclByText(funcName).get().getCtx();
+            //value  = visitFuncDecl(funcDeclContext);
+            visitChildren(funcDeclContext);
+
+            if (!(funcDeclContext.returnInstr().getChild(1).getText().equals("void"))) {
+                value = ((FunctionSymbol) result.get()).resolveByName(funcDeclContext.returnInstr().getChild(1).getText()).get().getValue();
+                System.out.println("TEst = " + value);
+            }
+
+            //super.visitFuncDecl(funcDeclContext);
+            //visitChildren(ctx);
+            System.out.println("SETTING SCOPE TO :" + currentScope.getScopeName());
+            this.symbolTable.setCurrentScope(currentScope);
+            symbolTable.getCurrentScope().resolveByName(ctx.getText()).get().setValue(value);
+        }
+        System.out.println("reutn visiFuncCall :" + value);
         return value;
     }
 
@@ -205,6 +322,41 @@ public class NbcPrinterVisitor extends PlayPlusBaseVisitor {
 
         return result;
 
+    }
+
+    /**
+     * Trouve une functionDecl qui match le text dans la table des functiondecls
+     * @param functText
+     * @return
+     */
+    private Optional<FunctionDecl> findFunctionDeclByText(String functText) {
+        Optional<FunctionDecl> functionDecl = this.functionDecls.stream()
+                .filter(x -> x.getText().equals(functText))
+                .findFirst();
+        return functionDecl;
+    }
+
+    private Optional<Symbol> resolveFunc(String funName) {
+        Scope currentScope = this.symbolTable.getCurrentScope();
+        System.out.println("currentscope : "+ currentScope.getScopeName());
+        Optional<Symbol> func = resolveSymbolRec(funName, currentScope);
+        if (! func.isPresent()) {
+            System.out.println("Function "+ funName +" do not exist");
+        }
+        return func;
+
+    }
+
+    private Optional<Symbol> resolveSymbolRec(String name, Scope currentScope) {
+        System.out.println("resolveSymbolRec : "+ currentScope.getScopeName());
+        Optional<Symbol> symbol = currentScope.resolveByName(name);
+        if (! symbol.isPresent() && currentScope.getEnclosingScope() == null) {
+            return Optional.empty();
+        }
+        if (symbol.isPresent()) {
+            return symbol;
+        }
+        return resolveSymbolRec(name, currentScope.getEnclosingScope());
     }
 
 
